324085ae316dd0e03b90cdc42985d4c98e6ddd42
Fernando Serra <fernando.serra.r@gmail.com>
2022-06-08 22:44:05 -0400

    Update MyService

--- app/src/main/java/com/example/androidbroadcastreceiver/ui/dashboard/MyService.kt
+++ app/src/main/java/com/example/androidbroadcastreceiver/ui/dashboard/MyService.kt
@@ -1,4 +1,30 @@
 package com.example.androidbroadcastreceiver.ui.dashboard
 
-class MyService {
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+
+class MyService: Service() {
+
+    init {
+        Log.i(TAG, "-> Services running")
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.i(TAG, "-> onCreate")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "-> onDestroy")
+    }
+
+    //Metodo que nos permite enlazar algun otro componente con nuestro servicio
+    override fun onBind(p0: Intent?): IBinder? = null
+
+    companion object{
+        const val TAG = "MyService"
+    }
 }
