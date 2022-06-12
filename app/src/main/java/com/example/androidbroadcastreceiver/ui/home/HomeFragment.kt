1f8df589a65dc595ec4cab82359e9ccc794e3dc4
Fernando Serra <fernando.serra.r@gmail.com>
2022-06-11 23:53:22 -0400

    Refactor Home

--- app/src/main/java/com/example/androidbroadcastreceiver/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/androidbroadcastreceiver/ui/home/HomeFragment.kt
@@ -14,8 +14,6 @@ class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?,   savedInstanceState: Bundle? ): View {
