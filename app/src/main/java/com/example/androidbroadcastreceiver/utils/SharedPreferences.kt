bdc2768ceb7ecd131ccdc0023e1e2bb496d7a6be
Fernando Serra <fernando.serra.r@gmail.com>
2022-06-14 10:42:30 -0400

    Update SharedPreferences

--- app/src/main/java/com/example/androidbroadcastreceiver/utils/SharedPreferences.kt
+++ app/src/main/java/com/example/androidbroadcastreceiver/utils/SharedPreferences.kt
@@ -1,4 +1,17 @@
 package com.example.androidbroadcastreceiver.utils
 
-class SharedPreferences {
+import android.content.Context
+
+class SharedPreferences (context: Context) {
+
+    private val sharedPreferences = context.getSharedPreferences("PreferencesApp",0)
+
+    fun putBoolean(Key:String, value: Boolean){
+        sharedPreferences.edit().putBoolean(Key,value).apply()
+    }
+
+    fun getBoolean(Key: String):Boolean{
+        return  sharedPreferences.getBoolean(Key,false)
+    }
+
 }
