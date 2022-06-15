bcba87663ae5c0201d7df888ca41b75551b3b24b
Fernando Serra <fernando.serra.r@gmail.com>
2022-06-15 12:41:27 -0400

    Update

--- app/src/main/java/com/example/androidbroadcastreceiver/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/androidbroadcastreceiver/ui/home/HomeFragment.kt
@@ -10,15 +10,18 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.androidbroadcastreceiver.databinding.FragmentHomeBinding
+import com.example.androidbroadcastreceiver.utils.SharedPreferences
 
 class HomeFragment : Fragment() {
+
+    private lateinit var preferencesProvider: SharedPreferences
 
     private var _binding: FragmentHomeBinding? = null
 
     private val binding get() = _binding!!
 
     override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?,   savedInstanceState: Bundle? ): View {
-
+        preferencesProvider= SharedPreferences(requireContext())
         val homeViewModel =  ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
@@ -33,7 +36,14 @@ class HomeFragment : Fragment() {
         }
 
         binding.btnValue.setOnClickListener {
-            Toast.makeText(context, "Almacenando valor", Toast.LENGTH_SHORT).show()
+            Toast.makeText(context, "Key::: ${binding.editTextKey.text}  Value::: ${binding.editTextValue.text}", Toast.LENGTH_SHORT).show()
+            preferencesProvider.putValue(binding.editTextKey.text.toString(), binding.editTextValue.text.toString())
+        }
+
+        binding.btnGetValue.setOnClickListener {
+            Log.i(TAG, "BUSCANDO Key::: ${binding.editTextKey.text} ")
+           val show = preferencesProvider.getValues(binding.editTextKey.text.toString()).toString()
+            Toast.makeText(context, "valor: $show", Toast.LENGTH_SHORT).show()
         }
 
         return root
