416783578283988088a3f1433965368265717c09
Fernando Serra <fernando.serra.r@gmail.com>
2022-06-25 12:24:56 -0400

    update service

--- app/src/main/java/com/example/androidbroadcastreceiver/Service.kt
+++ app/src/main/java/com/example/androidbroadcastreceiver/Service.kt
@@ -2,10 +2,13 @@ package com.example.androidbroadcastreceiver
 
 import android.app.Service
 import android.content.Intent
+import android.os.Handler
 import android.os.IBinder
 import android.util.Log
 
 class Service : Service() {
+
+    val handler= Handler()
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
@@ -13,11 +16,28 @@ class Service : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.d(TAG, ":::START::: ")
-        return super.onStartCommand(intent, flags, startId)
+
+        var counter =0
+
+        handler.apply {
+            var runnable= object : Runnable{
+                override fun run() {
+                    counter ++
+                    Log.d(TAG, "run: $counter")
+                    postDelayed(this, 1000)
+                }
+            }
+            postDelayed(runnable,1000)
+        }
+
+
+        return START_STICKY //si el sistema mata el servicio se vuelva a crear
+        //return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onDestroy() {
         Log.d(TAG, ":::DESTROY::: ")
+        handler.removeCallbacksAndMessages(null)
         super.onDestroy()
     }
     
